4a8f7861d7e45f264e833ec7f8367271347a7f88
dillontan19 <diqtran@ucsc.edu>
2024-10-21 09:33:11 -0700

    Session 2 Prompt for Coding Journal 1

---
+++ cj1-2.py
@@ -0,0 +1,16 @@
+
+a = 1.0
+b = 2.5
+
+c = 7
+d = 9
+
+def main():
+    print(a+b)
+    print(type(a+b))
+    print(d-c)
+    print(type(d-c))
+    print(b*float(c))
+    print(type(b*float(c)))
+if __name__=="__main__":
+    main()
