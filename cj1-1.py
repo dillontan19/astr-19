9774175a51c8791214531aa6e18060cad1535425
dillontan19 <diqtran@ucsc.edu>
2024-10-20 22:07:23 -0700

    Session 1 Prompt for Coding Journal 1.

---
+++ cj1-1.py
@@ -0,0 +1,5 @@
+def main():
+
+    print("My name is Dillon Tran (he/him). My favorite move is 'Everything, Everywhere, All at Once.' My favorite food is tied between sushi, poke, and ramen.")
+if __name__=="__main__":
+    main()
